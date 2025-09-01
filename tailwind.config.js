c758ef551b871a6a01c0935d0d0d6911dc4528ff
outreach-tech <outreach@getmvi.com>
2025-09-02 01:39:15 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+      },
+      colors: {
+        red: {
+          400: '#f87171',
+          500: '#dc2626',
+          600: '#dc2626',
+          700: '#b91c1c',
+        },
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.6s ease-out',
+        'slide-up': 'slideUp 0.6s ease-out',
+      },
+    },
+  },
+  plugins: [],
+};
